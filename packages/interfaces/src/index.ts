d64e5d57a78768283f65f1f864439c98ba1525fb
Krystian Kat <krystian.kat@protonmail.com>
2022-04-22 13:15:08 +0200

    refactor: delete double space

--- packages/interfaces/src/index.ts
+++ packages/interfaces/src/index.ts
@@ -1,37 +1,37 @@
 export interface ITransfer {
-  id?: string
-  type: "outgoing" | "incoming"
-  date: string
-  amount: number
-  fromOrToName?: string
+  id?: string;
+  type: 'outgoing' | 'incoming';
+  date: string;
+  amount: number;
+  fromOrToName?: string;
 }
 
-export interface  IAccountDetails {
-  accountBalance: number,
-  accountNumber: number,
+export interface IAccountDetails {
+  accountBalance: number;
+  accountNumber: number;
 }
 
-export interface  ITransferHistory {
-  transfers: ITransfer[]
+export interface ITransferHistory {
+  transfers: ITransfer[];
 }
 
 export interface IGetTransfersByDateRangeData {
-  from: string
-  to: string
+  from: string;
+  to: string;
 }
 
 export interface ISendTransferResponse {
-  isCorrect: boolean
-  transferID: number
-  accountBalance: number
-  message: string
+  isCorrect: boolean;
+  transferID: number;
+  accountBalance: number;
+  message: string;
 }
-
 
 export interface IBankAppInterface {
-  getAccountDetails(): () => Promise<IAccountDetails>
-  getTransfersHistory(): () => Promise<ITransferHistory>
-  getTransfersByDateRange(): (data: IGetTransfersByDateRangeData ) => Promise<ITransferHistory>
-  sendTransfer(): (data: ITransfer) => Promise<ISendTransferResponse>
+  getAccountDetails(): () => Promise<IAccountDetails>;
+  getTransfersHistory(): () => Promise<ITransferHistory>;
+  getTransfersByDateRange(): (
+    data: IGetTransfersByDateRangeData
+  ) => Promise<ITransferHistory>;
+  sendTransfer(): (data: ITransfer) => Promise<ISendTransferResponse>;
 }
-
