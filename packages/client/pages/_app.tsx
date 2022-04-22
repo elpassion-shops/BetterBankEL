e743be91e9f246005ef3aa0bc2a3210fb0fc2a64
hub33k <hub33k@yahoo.com>
2022-04-22 13:56:05 +0200

    feat: add config file (#3)

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,12 +1,13 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import config from '../config';
 import '../styles/tailwind.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>BetterBankEL</title>
+        <title>{config.appName}</title>
         <link rel="icon" type="image/svg+xml" href="data:image/x-icon;" />
       </Head>
 
--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,9 +1,11 @@
+import config from '../config';
+
 export function Index() {
   return (
     <>
       <div className="container mx-auto px-4">
         <h1 className="text-3xl dark:text-white font-bold underline">
-          BetterBankEL
+          {config.appName}
         </h1>
       </div>
     </>
