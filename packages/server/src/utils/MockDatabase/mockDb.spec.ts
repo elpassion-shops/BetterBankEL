78c5f6ac66957e5f5462a726ac6aa94ac652ecc0
mwakulinski <michal.wakulinski1@gmail.com>
2022-04-22 14:13:17 +0200

    it correctyl returns Account Details

--- packages/server/src/utils/MockDatabase/mockDb.spec.ts
+++ packages/server/src/utils/MockDatabase/mockDb.spec.ts
@@ -1,12 +1,29 @@
-class MockDataBase {}
+import { IAccountDetails } from '../../../../interfaces/src/index';
+
+class MockDataBase {
+  constructor(private readonly accountDetails: IAccountDetails) {}
+
+  getAccountDetails(): any {
+    return this.accountDetails;
+  }
+}
 
 describe('MockDataBase', () => {
   let mockDb: MockDataBase;
 
+  let mockAccountDetails: IAccountDetails = {
+    accountBalance: 1000,
+    accountNumber: 1234567890,
+  };
+
   beforeEach(() => {
-    mockDb = new MockDataBase();
+    mockDb = new MockDataBase(mockAccountDetails);
   });
   it('should create new instance of class', () => {
     expect(mockDb).toBeTruthy;
+  });
+
+  it('should return Account Details', () => {
+    expect(mockDb.getAccountDetails()).toEqual(mockAccountDetails);
   });
 });
