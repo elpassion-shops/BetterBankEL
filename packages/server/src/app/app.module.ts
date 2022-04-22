e4299b2ee3f3eca862e318186c135438a8aed96b
aleksander <kruk.aleksander@hotmail.com>
2022-04-22 15:08:12 +0200

    feat: create basic transfers endpoint

--- packages/server/src/app/app.module.ts
+++ packages/server/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountModule } from '../account/account.module';
+import { TransfersModule } from '../transfers/transfers.module';
 
 @Module({
-  imports: [AccountModule],
+  imports: [AccountModule, TransfersModule],
   controllers: [AppController],
   providers: [AppService],
 })
