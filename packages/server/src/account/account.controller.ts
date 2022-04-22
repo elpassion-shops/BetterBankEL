51660d9a5ae6736b08f821faa14a5cdef3920cfc
aleksander <kruk.aleksander@hotmail.com>
2022-04-22 15:02:04 +0200

    fix: account controller

--- packages/server/src/account/account.controller.ts
+++ packages/server/src/account/account.controller.ts
@@ -1,9 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AccountService } from './account.service';
 
 @Controller('account')
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
+  @Get('/')
   sendAccountDetails() {
     return this.accountService.sendAccountDetails();
   }
