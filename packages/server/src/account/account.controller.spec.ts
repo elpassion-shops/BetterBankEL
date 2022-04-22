a2f85cbbcfa9d2c4644151c3f4d7af243a3fc9ab
aleksander <kruk.aleksander@hotmail.com>
2022-04-22 14:06:51 +0200

    feat: controller pass should send account balance to a client

--- packages/server/src/account/account.controller.spec.ts
+++ packages/server/src/account/account.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AccountController } from './account.controller';
+import { AccountService } from './account.service';
 
 describe('AccountController', () => {
   let controller: AccountController;
@@ -7,6 +8,7 @@ describe('AccountController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AccountController],
+      providers: [AccountService],
     }).compile();
 
     controller = module.get<AccountController>(AccountController);
@@ -14,5 +16,10 @@ describe('AccountController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('should send balance to a client', function () {
+    expect(controller.sendAccountDetails()).toEqual({
+      accountBalance: expect.any(Number),
+    });
   });
 });
