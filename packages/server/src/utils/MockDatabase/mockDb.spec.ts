0cfec574324eb59229eb772d1be770d33a133ce1
mwakulinski <michal.wakulinski1@gmail.com>
2022-04-22 17:47:09 +0200

    should not subtract transfer amount from accountBalance when "outgoing" type passed and not enough money

--- packages/server/src/utils/MockDatabase/mockDb.spec.ts
+++ packages/server/src/utils/MockDatabase/mockDb.spec.ts
@@ -6,7 +6,7 @@ import {
 
 class MockDataBase {
   transfers: ITransfer[] = [];
-  constructor(private readonly accountDetails: IAccountDetails) {}
+  constructor(public accountDetails: IAccountDetails) {}
 
   getAccountDetails(): IAccountDetails {
     return this.accountDetails;
@@ -15,21 +15,25 @@ class MockDataBase {
   sendTransfer(mockTransfer: ITransfer): ISendTransferResponse {
     const transferId = Math.random().toString();
     this.transfers = [...this.transfers, { id: transferId, ...mockTransfer }];
+
     const accountBalance =
       mockTransfer.type === 'incoming'
         ? this.accountDetails.accountBalance + mockTransfer.amount
         : this.accountDetails.accountBalance - mockTransfer.amount;
+
+    const isCorrect = accountBalance >= 0 ? true : false;
+    const message =
+      accountBalance >= 0
+        ? 'ok'
+        : "Something went wrong we didn't charge any money";
+
+    this.accountDetails.accountBalance =
+      accountBalance >= 0 ? accountBalance : this.accountDetails.accountBalance;
     return {
-      isCorrect: accountBalance >= 0 ? true : false,
-      accountBalance:
-        accountBalance >= 0
-          ? accountBalance
-          : this.accountDetails.accountBalance,
+      isCorrect,
+      accountBalance: this.accountDetails.accountBalance,
       transferID: transferId,
-      message:
-        accountBalance >= 0
-          ? 'Ok'
-          : "Something went wrong we didn't charge any money",
+      message,
     };
   }
 
@@ -40,11 +44,7 @@ class MockDataBase {
 
 describe('MockDataBase', () => {
   let mockDb: MockDataBase;
-
-  let mockAccountDetails: IAccountDetails = {
-    accountBalance: 1000,
-    accountNumber: '1234567890',
-  };
+  let mockAccountDetails: IAccountDetails;
 
   let mockTransfer: ITransfer = {
     type: 'incoming',
@@ -61,8 +61,13 @@ describe('MockDataBase', () => {
   };
 
   beforeEach(() => {
+    mockAccountDetails = {
+      accountBalance: 1000,
+      accountNumber: '1234567890',
+    };
     mockDb = new MockDataBase(mockAccountDetails);
   });
+
   it('should create new instance of class', () => {
     expect(mockDb).toBeTruthy;
   });
@@ -76,7 +81,7 @@ describe('MockDataBase', () => {
       isCorrect: true,
       accountBalance: 2000,
       transferID: expect.any(String),
-      message: 'Ok',
+      message: 'ok',
     });
   });
 
@@ -85,7 +90,17 @@ describe('MockDataBase', () => {
       isCorrect: true,
       accountBalance: 0,
       transferID: expect.any(String),
-      message: 'Ok',
+      message: 'ok',
+    });
+  });
+
+  it('should not subtract transfer amount from accountBalance when "outgoing" type passed and not enough money', () => {
+    mockDb.sendTransfer(mockTransferMinus);
+    expect(mockDb.sendTransfer(mockTransferMinus)).toEqual({
+      isCorrect: false,
+      accountBalance: 0,
+      transferID: expect.any(String),
+      message: "Something went wrong we didn't charge any money",
     });
   });
 
