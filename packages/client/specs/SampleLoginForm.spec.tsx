ee3a768bdf41e0b0f3237c66be6198312d295888
Krystian Kat <krystian.kat@protonmail.com>
2022-04-23 16:29:01 +0200

    feat: add sampleLoginForm to show how msw works

---
+++ packages/client/specs/SampleLoginForm.spec.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom/extend-expect';
+import 'whatwg-fetch';
+import { mswServer } from '../mocks/mswServer';
+import LoginForm from '../pages/SampleLoginForm';
+
+beforeAll(() => mswServer.listen());
+afterEach(() => mswServer.resetHandlers());
+afterAll(() => mswServer.close());
+
+describe('LoginForm', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<LoginForm />);
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('should allow a user to log in', async () => {
+    const accountBalance = '42';
+    const accountNumber = '61109010140000071219812874';
+
+    render(<LoginForm />);
+
+    await userEvent.type(screen.getByLabelText(/username/i), 'johnUser');
+
+    await userEvent.click(screen.getByRole('button', { name: /submit/i }));
+
+    expect(
+      await screen.findByText('f79e82e8-c34a-4dc7-a49e-9fadc0979fda')
+    ).toBeInTheDocument();
+    expect(await screen.findByText('John')).toBeInTheDocument();
+    expect(await screen.findByText('Maverick')).toBeInTheDocument();
+    expect(await screen.findByText(accountBalance)).toBeInTheDocument();
+    expect(await screen.findByText(accountNumber)).toBeInTheDocument();
+  });
+});
