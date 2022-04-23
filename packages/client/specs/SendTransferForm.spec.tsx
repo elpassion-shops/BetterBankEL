1b161f711a92ed415e566dd39b9a9e7e77a8426a
Krystian Kat <krystian.kat@protonmail.com>
2022-04-23 23:24:02 +0200

    test: add tests for SendTransferForm fields typing

--- packages/client/specs/SendTransferForm.spec.tsx
+++ packages/client/specs/SendTransferForm.spec.tsx
@@ -6,6 +6,7 @@ import { mswServer } from '../mocks/mswServer';
 import SendTransferForm from '../components/SendTransferForm';
 import { expect } from '@jest/globals';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 
 beforeAll(() => mswServer.listen());
 afterEach(() => mswServer.resetHandlers());
@@ -73,6 +74,64 @@ describe(SendTransferForm.name, () => {
 
       // then
       expect(transferDateInputElement.value).toBe('');
+    });
+  });
+
+  describe('tests for SendTransferForm fields typing', () => {
+    it('should be able to type account number', async () => {
+      // given
+      const bankAccountNumber = '61109010140000071219812874';
+      const accountNumberInputElement = context.getByPlaceholderText(
+        /To account number/i
+      ) as HTMLInputElement;
+
+      // when
+      await userEvent.type(accountNumberInputElement, bankAccountNumber);
+
+      // then
+      expect(accountNumberInputElement.value).toBe(bankAccountNumber);
+    });
+
+    it('should be able to type title for transfer', async () => {
+      // given
+      const transferTitle = 'Wypłata z EL Passion';
+      const transferTitleInputElement = context.getByPlaceholderText(
+        /Title of transfer/i
+      ) as HTMLInputElement;
+
+      // when
+      await userEvent.type(transferTitleInputElement, transferTitle);
+
+      // then
+      expect(transferTitleInputElement.value).toBe(transferTitle);
+    });
+
+    it('input for "From the account" should by initially empty', async () => {
+      // given
+      const fromAccountInputElement = context.getByPlaceholderText(
+        /From account/i
+      ) as HTMLInputElement;
+      const fromAccount = '61109010140000071219812874';
+
+      // when
+      await userEvent.type(fromAccountInputElement, fromAccount);
+
+      // then
+      expect(fromAccountInputElement.value).toBe(fromAccount);
+    });
+
+    it('input for "Transfer date" should by initially empty', async () => {
+      // given
+      const transferDate = '2011-10-05T14:48:00.000Z';
+      const transferDateInputElement = context.getByPlaceholderText(
+        /Transfer date/i
+      ) as HTMLInputElement;
+
+      // when
+      await userEvent.type(transferDateInputElement, transferDate);
+
+      // then
+      expect(transferDateInputElement.value).toBe(transferDate);
     });
   });
 });
