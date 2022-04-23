f96ecf789a5c159663d8b74557f9ee0f1f05752f
Krystian Kat <krystian.kat@protonmail.com>
2022-04-23 23:21:44 +0200

    test: add tests for SendTransferForm initially fields

--- packages/client/specs/SendTransferForm.spec.tsx
+++ packages/client/specs/SendTransferForm.spec.tsx
@@ -33,4 +33,46 @@ describe(SendTransferForm.name, () => {
       expect(addButtonElement).not.toBeNull();
     });
   });
+
+  describe('tests for SendTransferForm initially fields', () => {
+    it('input for "To account number" should by initially empty', async () => {
+      // given
+      const accountNumberInputElement = context.getByPlaceholderText(
+        /To account number/i
+      ) as HTMLInputElement;
+
+      // then
+      expect(accountNumberInputElement.value).toBe('');
+    });
+
+    it('input for "Title of transfer" should by initially empty', async () => {
+      // given
+      const transferTitleInputElement = context.getByPlaceholderText(
+        /Title of transfer/i
+      ) as HTMLInputElement;
+
+      // then
+      expect(transferTitleInputElement.value).toBe('');
+    });
+
+    it('input for "From the account" should by initially empty', async () => {
+      // given
+      const fromAccountElementElement = context.getByPlaceholderText(
+        /From account/i
+      ) as HTMLInputElement;
+
+      // then
+      expect(fromAccountElementElement.value).toBe('');
+    });
+
+    it('input for "Transfer date" should by initially empty', async () => {
+      // given
+      const transferDateInputElement = context.getByPlaceholderText(
+        /Transfer date/i
+      ) as HTMLInputElement;
+
+      // then
+      expect(transferDateInputElement.value).toBe('');
+    });
+  });
 });
