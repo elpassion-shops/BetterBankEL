47574a471db68a3c35ec211891dce4a07a7104eb
mwakulinski <michal.wakulinski1@gmail.com>
2022-04-24 10:17:59 +0200

    setup typeorm config and typeorm connection

---
+++ packages/server/src/app/database/typeorm.config.ts
@@ -0,0 +1,68 @@
+import { User } from './entities/User.entity';
+
+module.exports = [
+  {
+    name: 'default', //for all environments
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    schema: 'public',
+    synchronize: false,
+    migrationsRun: true,
+
+    logging: process.env.DATABASE_LOGGING === 'true',
+
+    autoLoadEntities: true,
+
+    entities: [User],
+    migrationsTableName: 'migrations',
+    migrations: ['dist/**/migrations/*.js'],
+    cli: {
+      migrationsDir: './src/database/migrations',
+    },
+  },
+  {
+    name: 'production', //for all environments
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    schema: 'public',
+    ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    synchronize: false,
+    migrationsRun: true,
+
+    logging: process.env.DATABASE_LOGGING === 'true',
+
+    autoLoadEntities: true,
+
+    entities: [User],
+    migrationsTableName: 'migrations',
+    migrations: ['dist/**/migrations/*.js'],
+    cli: {
+      migrationsDir: './src/database/migrations',
+    },
+  },
+  {
+    name: 'test',
+    type: 'postgres',
+    schema: 'public',
+
+    synchronize: false,
+    migrationsRun: true,
+
+    logging: process.env.DATABASE_LOGGING === 'true',
+
+    autoLoadEntities: true,
+
+    entities: ['./src/**/*.entity.ts'], // tests run on TS directly
+    migrations: ['./src/**/migrations/*.ts'],
+    migrationsTableName: 'migrations',
+
+    cli: {
+      migrationsDir: './src/database/migrations',
+    },
+  },
+];
