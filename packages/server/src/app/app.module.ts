80d2a8b228e00af919ec637e3a147be0f1815642
mwakulinski <michal.wakulinski1@gmail.com>
2022-04-24 10:27:05 +0200

    setup modules

--- packages/server/src/app/app.module.ts
+++ packages/server/src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountModule } from './account/account.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [AccountModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        `.env.${process.env.NODE_ENV || 'development'}.local`,
+        '.env',
+        `.env.${process.env.NODE_ENV || 'development'}`,
+      ],
+      cache: true,
+      isGlobal: true,
+    }),
+    AccountModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
