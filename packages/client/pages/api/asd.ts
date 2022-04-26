f712c14c80a4100693a669dce128eb5378dc798f
Krystian Kat <krystian.kat@protonmail.com>
2022-04-26 14:57:02 +0200

    feat: try and cry

---
+++ packages/client/pages/api/asd.ts
@@ -0,0 +1,49 @@
+import { Request, Response, NextFunction } from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import { getSession } from 'next-auth/react';
+
+export class Proxy {
+  public get() {
+    return createProxyMiddleware({
+      target: process.env.API_URL,
+      changeOrigin: true,
+      secure: false,
+    });
+  }
+}
+
+const runMiddleware = (
+  req: Request,
+  res: Response,
+  fn: (req: Request, res: Response, fn: NextFunction) => void
+) =>
+  new Promise((resolve, reject) => {
+    fn(req, res, (result: unknown) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+
+const proxy = new Proxy();
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+async function handler(req: any, res: any) {
+  const session = await getSession({ req });
+  if (session) {
+    console.log(session);
+    req.headers['Authorization'] = `Bearer ${session.accessToken}`;
+  }
+  // TUTAJ IDZIE KOD DODAJĄCY CIASTKO
+  await runMiddleware(req, res, proxy.get());
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default handler;
--- packages/client/pages/login.tsx
+++ packages/client/pages/login.tsx
@@ -13,7 +13,7 @@ export default function Login() {
   return (
     <>
       Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <button onClick={() => signIn('auth0')}>Sign in</button>
     </>
   );
 }
