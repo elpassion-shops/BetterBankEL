8a40bd717936a335c5adc96a6e94856f9a9b1129
Krystian Kat <krystian.kat@protonmail.com>
2022-04-26 11:00:50 +0200

    refactor: add data-tesid

--- packages/client/components/AccountDetails.tsx
+++ packages/client/components/AccountDetails.tsx
@@ -4,12 +4,19 @@ export default function AccountDetails() {
   return (
     <>
       <div className="bg-white	p-4 text-black  ">
-        <div className="text-lg font-bold mb-3">eKonto</div>
-        <div className="font-extralight">Available funds</div>
-        <div className="mb-3">
-          323,00<span className="text-xs font-normal	">PLN</span>
+        <div data-testid="accountType" className="text-lg font-bold mb-3">
+          eKonto
         </div>
-        <div className="font-extralight">
+        <div data-testid="availableFounds" className="font-extralight">
+          Available funds
+        </div>
+        <div className="mb-3">
+          323,00
+          <span data-testid="foundsCurrency" className="text-xs font-normal	">
+            PLN
+          </span>
+        </div>
+        <div data-testid="upcomingPayments" className="font-extralight">
           Upcoming payments
           <span className="text-xs font-normal	"> 0,00 PLN</span>
         </div>
