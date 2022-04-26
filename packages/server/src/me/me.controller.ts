0d803980c6d4dd4140e70fd296daa3bc735a8f26
aleksander <kruk.aleksander@hotmail.com>
2022-04-26 10:46:50 +0200

    feat: create jwt auth guard

--- packages/server/src/me/me.controller.ts
+++ packages/server/src/me/me.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get, Request, Response } from '@nestjs/common';
+import { MeService } from './me.service';
 
 @Controller('me')
 export class MeController {
+  constructor(private readonly meService: MeService) {}
   @Get('/')
   checkIsLogged(@Request() req, @Response() res) {
-    if (req.cookies['isDupa'] === 'yesItIS')
-      return res.status(200).send({ msg: 'You are logged in' });
-    return res.status(401).send({ msg: 'Not logged in' });
+    return this.meService.me();
   }
 }
--- packages/server/src/me/me.module.ts
+++ packages/server/src/me/me.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MeController } from './me.controller';
+import { MeService } from './me.service';
+import { JwtStrategy } from '../auth/jwt.strategy';
 
 @Module({
-  controllers: [MeController]
+  controllers: [MeController],
+  providers: [MeService, JwtStrategy],
 })
 export class MeModule {}
