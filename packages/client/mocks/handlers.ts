e870e5877d19f11982886cca3ea04ee85cda672e
hub33k <hub33k@yahoo.com>
2022-04-26 21:07:01 +0200

    fix: restore /login mock

--- packages/client/mocks/handlers.ts
+++ packages/client/mocks/handlers.ts
@@ -140,19 +140,19 @@ export const handlers = [
     );
   }),
 
-  // rest.post<login>(
-  //   `${process.env.NEXT_PUBLIC_SERVER}/login`,
-  //   (req, res, ctx) => {
-  //     const { username } = req.body;
+  rest.post<login>(
+    `${process.env.NEXT_PUBLIC_SERVER}/login`,
+    (req, res, ctx) => {
+      const { username } = req.body;
 
-  //     return res(
-  //       ctx.json({
-  //         id: 'f79e82e8-c34a-4dc7-a49e-9fadc0979fda',
-  //         username,
-  //         firstName: 'John',
-  //         lastName: 'Maverick',
-  //       })
-  //     );
-  //   }
-  // ),
+      return res(
+        ctx.json({
+          id: 'f79e82e8-c34a-4dc7-a49e-9fadc0979fda',
+          username,
+          firstName: 'John',
+          lastName: 'Maverick',
+        })
+      );
+    }
+  ),
 ];
