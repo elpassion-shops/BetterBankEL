b1589a9c3020b600e19bd4aae0ab4d70f50ee087
aleksander <kruk.aleksander@hotmail.com>
2022-04-26 10:51:41 +0200

    feat: use jwt guard in me endpoint

--- packages/server/src/me/me.controller.ts
+++ packages/server/src/me/me.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Request, Response } from '@nestjs/common';
+import { Controller, Get, Request, Response, UseGuards } from '@nestjs/common';
 import { MeService } from './me.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('me')
 export class MeController {
   constructor(private readonly meService: MeService) {}
+  @UseGuards(JwtAuthGuard)
   @Get('/')
-  checkIsLogged(@Request() req, @Response() res) {
-    return this.meService.me();
+  checkIsLogged(@Request() req) {
+    return this.meService.me(req);
   }
 }
--- packages/server/src/me/me.service.ts
+++ packages/server/src/me/me.service.ts
@@ -2,5 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class MeService {
-  async me() {}
+  me(req) {
+    return { user: req.user };
+  }
 }
