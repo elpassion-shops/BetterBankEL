830cad2f9ecad833840a30834c0fbdf3336bb392
mwakulinski <michal.wakulinski1@gmail.com>
2022-04-26 10:51:57 +0200

    refactor: remove unused imports

--- packages/server/src/app/database/entities/Account.entity.ts
+++ packages/server/src/app/database/entities/Account.entity.ts
@@ -1,7 +1,5 @@
 import { IAccountDetails } from '@bank-el/interfaces';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-import { Transfer } from './Transfer.entity';
-import { User } from './User.entity';
 
 @Entity({ name: 'accounts' })
 export class Account implements IAccountDetails {
