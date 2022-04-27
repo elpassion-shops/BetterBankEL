ec9994087fab2c7476a5a65cd0e1393b51031526
mwakulinski <michal.wakulinski1@gmail.com>
2022-04-28 00:20:18 +0200

    add dependencies to modules

--- packages/server/src/login/login.module.ts
+++ packages/server/src/login/login.module.ts
@@ -2,8 +2,23 @@ import { Module } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { LoginController } from './login.controller';
 import { JwtStrategy } from '../auth/jwt.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from '../app/database/entities/Account.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from '../auth/constants';
+import { AccountModule } from '../app/account/account.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Account]),
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+    AccountModule,
+  ],
   providers: [LoginService, JwtStrategy],
   controllers: [LoginController],
 })
