4524e5ffef93dd622fc8e9ffef277993e29cb13f
Krystian Kat <krystian.kat@protonmail.com>
2022-04-27 16:33:13 +0200

    feat: implement dumb validation

--- packages/client/components/SendTransferForm.tsx
+++ packages/client/components/SendTransferForm.tsx
@@ -1,4 +1,16 @@
 import { ISendTransferResponse, ITransfer } from '@bank-el/interfaces';
+import { classValidatorResolver } from '@hookform/resolvers/class-validator';
+import {
+  IsDate,
+  IsIBAN,
+  IsInt,
+  IsString,
+  Max,
+  MaxLength,
+  Min,
+  MinDate,
+  MinLength,
+} from 'class-validator';
 import { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { BankAppApiContext } from '../pages';
@@ -14,6 +26,41 @@ export interface ITransferFormData {
   transferTitle: string;
 }
 
+export class SendTransferValidation implements ITransferFormData {
+  @IsString()
+  @MaxLength(35, { message: 'type less than 35 letters' })
+  @MinLength(5, { message: 'type more than 5 letters' })
+  receiverAddress: string;
+
+  @IsIBAN({ message: 'please enter a valid IBAN account number' })
+  receiverBankAccountNumber: string;
+
+  @IsString()
+  @MaxLength(35, { message: 'type less than 35 letters' })
+  @MinLength(5, { message: 'type more than 5 letters' })
+  recipientName: string;
+
+  @IsIBAN({ message: 'please enter a valid IBAN account number' })
+  senderBankAccountNumber: string;
+
+  @Max(1000000, { message: 'enter an amount less than 1000000' })
+  @Min(1, {
+    message: 'enter an amount greater than 1',
+  })
+  transferAmount: number;
+
+  @MinDate(new Date(), { message: "select today's date or a later date" })
+  @IsDate({
+    message: 'select the date of the transfer',
+  })
+  transferDate: Date;
+
+  @IsString()
+  @MaxLength(50, { message: 'type less than 50 letters' })
+  @MinLength(5, { message: 'type more than 5 letters' })
+  transferTitle: string;
+}
+
 export default function SendTransfer() {
   const { BankAppAPI } = useContext(BankAppApiContext);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -25,7 +72,9 @@ export default function SendTransfer() {
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm();
+  } = useForm<SendTransferValidation>({
+    resolver: classValidatorResolver(SendTransferValidation),
+  });
 
   const onSubmit = (data: ITransferFormData) => {
     console.log(data);
@@ -37,7 +86,7 @@ export default function SendTransfer() {
       address: data.receiverAddress || '',
       sender: '',
       senderIBAN: data.senderBankAccountNumber,
-      receiver: '',
+      receiver: data.recipientName,
       receiverIBAN: data.receiverBankAccountNumber,
     };
 
@@ -54,26 +103,34 @@ export default function SendTransfer() {
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col overflow-hidden relative z-0 "
       >
-        <div className="relative z-0 outline outline-transparent w-full mb-5 px-2 pl-4 pt-4 border border-b-2 rounded-[2px] border-b-gray-400 focus-within:border-blue-500">
+        <div className="mb-4">
+          <div className="relative z-0 outline outline-transparent w-full px-2 pl-4 pt-4 border border-b-2 rounded-[2px] border-b-gray-400 focus-within:border-blue-500">
+            <input
+              type="text"
+              name="recipientName"
+              id="recipientName"
+              placeholder=" "
+              className=" text-gray-800 pt-2 pb-2 block w-full px-0 mt-0 bg-transparent border-0 appearance-none focus:outline-none focus:ring-0"
+              {...register('recipientName', {})}
+            />
+            <label
+              htmlFor="recipientName"
+              className="absolute duration-300 top-4 -z-1 origin-0 text-gray-500 text-lg"
+            >
+              Recipient
+            </label>
+          </div>
+          <div className="pl-4"></div>
+        </div>
+        {errors.recipientName && (
+          <span role="alert" className="text-sm text-red-600">
+            {errors.recipientName.message}
+          </span>
+        )}
+
+        <div className="mt-2 relative z-0 outline outline-transparent w-full mb-5 px-2 pl-4 pt-4 border border-b-2 rounded-[2px] border-b-gray-400 focus-within:border-blue-500">
           <input
             type="text"
-            name="recipientName"
-            id="recipientName"
-            placeholder=" "
-            className=" text-gray-800 pt-2 pb-2 block w-full px-0 mt-0 bg-transparent border-0 appearance-none focus:outline-none focus:ring-0"
-            {...register('recipientName', {})}
-          />
-          <label
-            htmlFor="recipientName"
-            className="absolute duration-300 top-4 -z-1 origin-0 text-gray-500 text-lg"
-          >
-            Recipient
-          </label>
-        </div>
-
-        <div className="relative z-0 outline outline-transparent w-full mb-5 px-2 pl-4 pt-4 border border-b-2 rounded-[2px] border-b-gray-400 focus-within:border-blue-500">
-          <input
-            type="number"
             name="senderBankAccountNumber"
             id="senderBankAccountNumber"
             placeholder=" "
@@ -105,21 +162,28 @@ export default function SendTransfer() {
           </label>
         </div>
 
-        <div className="relative z-0 outline outline-transparent w-full mb-5 px-2 pl-4 pt-4 border border-b-2 rounded-[2px] border-b-gray-400 focus-within:border-blue-500">
-          <input
-            type="text"
-            name="receiverBankAccountNumber"
-            id="receiverBankAccountNumber"
-            placeholder=" "
-            className=" text-gray-800 pt-2 pb-2 block w-full px-0 mt-0 bg-transparent border-0 appearance-none focus:outline-none focus:ring-0"
-            {...register('receiverBankAccountNumber', {})}
-          />
-          <label
-            htmlFor="receiverBankAccountNumber"
-            className="absolute duration-300 top-4 -z-1 origin-0 text-gray-500 text-lg"
-          >
-            To account number
-          </label>
+        <div className="mb-5">
+          <div className="relative z-0 outline outline-transparent w-full  px-2 pl-4 pt-4 border border-b-2 rounded-[2px] border-b-gray-400 focus-within:border-blue-500">
+            <input
+              type="text"
+              name="receiverBankAccountNumber"
+              id="receiverBankAccountNumber"
+              placeholder=" "
+              className=" text-gray-800 pt-2 pb-2 block w-full px-0 mt-0 bg-transparent border-0 appearance-none focus:outline-none focus:ring-0"
+              {...register('receiverBankAccountNumber', {})}
+            />
+            <label
+              htmlFor="receiverBankAccountNumber"
+              className="absolute duration-300 top-4 -z-1 origin-0 text-gray-500 text-lg"
+            >
+              To account number
+            </label>
+          </div>
+          {errors.receiverBankAccountNumber && (
+            <span role="alert" className="text-sm text-red-600">
+              {errors.receiverBankAccountNumber.message}
+            </span>
+          )}
         </div>
 
         <div className="relative z-0 outline outline-transparent w-full mb-5 px-2 pl-4 pt-4 border border-b-2 rounded-[2px] border-b-gray-400 focus-within:border-blue-500">
