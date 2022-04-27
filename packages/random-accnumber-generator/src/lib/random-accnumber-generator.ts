2fd848a22e2eaeda5f57adb845a9f75fc4369f33
mwakulinski <michal.wakulinski1@gmail.com>
2022-04-28 00:20:47 +0200

    create AccountGenerator

--- packages/random-accnumber-generator/src/lib/random-accnumber-generator.ts
+++ packages/random-accnumber-generator/src/lib/random-accnumber-generator.ts
@@ -1,5 +1,23 @@
+import { IAccountDetails } from '@bank-el/interfaces';
+
 export function randomAccnumberGenerator(): string {
   return `491020289222763005${Math.floor(
     Math.random() * 100000000
   ).toString()}`;
+}
+
+export class AccountGenerator {
+  static generateRandomNumber() {
+    return `491020289222763005${Math.floor(
+      Math.random() * 100000000
+    ).toString()}`;
+  }
+
+  static createAccount(email: string): IAccountDetails {
+    return {
+      email: email,
+      accountBalance: 1000,
+      accountNumber: this.generateRandomNumber(),
+    };
+  }
 }
