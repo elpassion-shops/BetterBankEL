a1d6c5462aa74e4931777467aca4121dc2d7e087
mwakulinski <michal.wakulinski1@gmail.com>
2022-04-27 15:30:13 +0200

    refactor: change way of importing cookieParser

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -2,7 +2,7 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
