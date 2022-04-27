85ac824d103e56971860f5fb64bdaa88a16a84a1
aleksander <kruk.aleksander@hotmail.com>
2022-04-27 10:25:26 +0200

    fix:  id come back :)

--- packages/dto-shared/src/lib/Account.dto.ts
+++ packages/dto-shared/src/lib/Account.dto.ts
@@ -1,6 +1,15 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class AccountDetailsDto {
+  @IsEmail()
+  @IsOptional()
+  id?: string;
   @IsNumber()
   @IsNotEmpty()
   accountBalance: number;
