99c3fad75d63ed6ed919ba56fb66d7a68b36f388
aleksander <kruk.aleksander@hotmail.com>
2022-04-28 10:05:41 +0200

    feat:  write logic for login service

--- packages/server/src/login/login.service.ts
+++ packages/server/src/login/login.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Account } from '../app/database/entities/Account.entity';
+import { AccountDetailsDto } from '@bank-el/dto-shared';
 
 @Injectable()
 export class LoginService {
-  async login(req) {
-    return { status: 200, userData: req.user };
+  constructor(
+    @InjectRepository(Account)
+    private accountRepository: Repository<Account>
+  ) {}
+
+  async login(req): Promise<AccountDetailsDto | { status: number; err }> {
+    try {
+      const user = await this.accountRepository.findOne({
+        email: req.user.login,
+      });
+      return {
+        accountBalance: user.accountBalance,
+        accountNumber: user.accountNumber,
+      };
+    } catch (err) {
+      return { status: 500, err };
+    }
   }
 }
