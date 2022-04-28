7774f2bc447e6565d75f37fde6686a50f061cf85
hub33k <hub33k@yahoo.com>
2022-04-28 09:36:07 +0200

    feat: create UserContext

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import UserContext from '../providers/UserContext';
 
 export default function Index() {
+  const { getSession } = useContext(UserContext);
+
+  useEffect(() => {
+    fetch();
+
+    async function fetch() {
+      const data = await getSession();
+      if (data) {
+        console.log('Hello user');
+      } else {
+        console.log('Hello guest');
+      }
+    }
+  }, []);
+
   return (
     <>
       <p>Home page</p>
