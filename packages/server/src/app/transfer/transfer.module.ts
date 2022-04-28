ef0fb5ccc8ee435b185bd4109e358479734a7097
mwakulinski <michal.wakulinski1@gmail.com>
2022-04-28 10:35:40 +0200

    implements getAllTransfers

--- packages/server/src/app/transfer/transfer.module.ts
+++ packages/server/src/app/transfer/transfer.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from '../database/entities/Account.entity';
 import { Transfer } from '../database/entities/Transfer.entity';
 import { TransferController } from './transfer.controller';
 import { TransferService } from './transfer.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transfer])],
+  imports: [TypeOrmModule.forFeature([Transfer, Account])],
   controllers: [TransferController],
   providers: [TransferService],
 })
